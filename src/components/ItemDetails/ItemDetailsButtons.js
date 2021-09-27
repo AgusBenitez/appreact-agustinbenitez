ae29b584dabd5829bfba1da9b0885d498a61711a
AgusBenitez <agnbenitez1@gmail.com>
2021-09-27 19:34:37 -0300

    Corrección último commit. Botón agregar al carrito navega a ruta /cart

--- src/components/ItemDetails/ItemDetailsButtons.js
+++ src/components/ItemDetails/ItemDetailsButtons.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ItemDetailsButtons = () => {
     return (
         <div className='itemDetailsButtons'>
-            <a href='#' className='agregarCarrito'>Agregar al carrito</a>
+            <Link to="/cart" className="agregarCarrito">Agregar al carrito</Link>
             <a href='#' className='volverBtn'>Volver</a>
         </div>
     )
